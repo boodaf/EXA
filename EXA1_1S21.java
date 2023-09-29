a3bf9a12582563ed823387dbe38eb7a09a9d4b06
boodaf <144178847+boodaf@users.noreply.github.com>
2023-09-28 22:18:55 -0600

    Add files via upload

---
+++ EXA1_1S21.java
@@ -0,0 +1,15 @@
+  import java.util.Scanner;
+       public static void main(String[] args) {
+          Scanner teclado=new Scanner(System.in);
+        System.out.println("1.-Escribir un programa que escriba un rectangulo de 4*9 asteriscos,utilizando el caracter asteriscos(*)");
+        System.out.println();
+        System.out.println("****");
+        System.out.println("*  *");
+        System.out.println("*  *");
+        System.out.println("*  *");
+        System.out.println("*  *");
+        System.out.println("*  *");
+        System.out.println("*  *");
+        System.out.println("*  *");
+        System.out.println("****");
+        System.out.println();
